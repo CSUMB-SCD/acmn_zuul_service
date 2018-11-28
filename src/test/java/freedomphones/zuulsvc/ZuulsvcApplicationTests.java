e88752255c9856bd36983890572bbeb54559c298
cforsythe <cforsythe.cs@gmail.com>
2018-11-28 02:41:42 -0800

    zuulsvc set up

---
+++ src/test/java/freedomphones/zuulsvc/ZuulsvcApplicationTests.java
@@ -0,0 +1,16 @@
+package freedomphones.zuulsvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulsvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
